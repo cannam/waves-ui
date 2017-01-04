cc5e6f0dc6e3257d4f71b6d682ddac7069ad43ed
Lucas Thompson <dev@lucas.im>
2017-01-04 15:43:09 +0000

    Start a Shape for Spectrogram. Currently empty.

---
+++ src/shapes/spectrogram-shape.js
@@ -0,0 +1,18 @@
+/**
+ * Created by lucast on 04/01/2017.
+ */
+export default class Spectrogram extends BaseShape {
+    getClassName() {
+        return 'spectrogram';
+    }
+
+    // TODO determine suitable implementations for _getAccessorList and _getDefaults
+
+    render(ctx) {
+
+    }
+
+    update(ctx, datum) {
+
+    }
+}
